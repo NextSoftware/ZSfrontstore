d6fe30003078749efaf92c3926ae2f44413bda83
Leandro Gravito <113536943+LeandroGrav@users.noreply.github.com>
2023-09-15 09:57:02 +0000

    out

---
+++ out/_next/static/chunks/pages/user/orders-d7d3804b480c4132.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[122],{3224:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/user/orders",function(){return u(8821)}])}},function(n){n.O(0,[630,828,494,795,169,821,774,888,179],function(){return n(n.s=3224)}),_N_E=n.O()}]);
