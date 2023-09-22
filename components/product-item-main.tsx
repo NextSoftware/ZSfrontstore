2ad273e211565f0a32d69030895d082eea580674
LeandroGrav <leandro@nextsoftware.pt>
2023-09-22 11:14:07 +0000

    productItem fucntionality added

--- components/product-item-main.tsx
+++ components/product-item-main.tsx
@@ -18,44 +18,37 @@ const ProductItemMain = (data: any) => {
   const router = useRouter();
   const cartState = useSelector(selectCartState);
   const dispatch = useDispatch();
+  const addToCartHandler = async (article: any) => {
+    let arr = [];
+    //STOCK
+    if (data.prodstock == 0) {
+      return alert("sem stock");
+    }
 
-  // const addToCartHandler = async (article: any) => {
-  //   let arr = [];
-  //   await axios
-  //     .get(`/stock/findItemID/${article.Product.Item_ID}`)
-  //     .then(async (response) => {
-  //       arr = await response.data;
-  //     });
-  //   if (arr.length == 0) {
-  //     return alert("sem stock");
-  //   }
-  //   const index = cartState
-  //     .map((x: any) => {
-  //       return x.id;
-  //     })
-  //     .indexOf(article.Product.id);
-  //   let imgLocation = "";
-  //   if (index <= -1) {
-  //     if (article.Image_ID.length > 0) {
-  //       imgLocation = article.Image_ID[0].location;
-  //     } else {
-  //       imgLocation = "";
-  //     }
-  //     dispatch(
-  //       cartAddItem({
-  //         id: article.Product.id,
-  //         item_ID: article.Product.Item_ID,
-  //         name: article.Product.Name,
-  //         qty: 1,
-  //         price: article.Product.Price,
-  //         description: article.Product.Description,
-  //         image: imgLocation,
-  //       })
-  //     );
-  //   } else {
-  //     dispatch(cartIncrementItem({ id: article.Product.id, qty: 1 }));
-  //   }
-  // };
+    const index = cartState
+      .map((x: any) => {
+        return x.id;
+      })
+      .indexOf(article.codigo);
+    if (index <= -1) {
+      dispatch(
+        cartAddItem({
+          id: article.codigo,
+          item_ID: article.descricao,
+          name: article.descricao,
+          qty: 1,
+          price: article.precovenda,
+          description: article.descricao,
+          image: article?.foto,
+        })
+      );
+    } else {
+      dispatch(cartIncrementItem({ id: article.codigo, qty: 1 }));
+    }
+    // setCartMessage(true);
+    // await new Promise((resolve) => setTimeout(resolve, 0)); // Wait for state to update
+    // setCartMessage(false);
+  };
   function addCartMessage() {
     setAddCart(false);
   }
@@ -74,11 +67,12 @@ const ProductItemMain = (data: any) => {
                 }
                 className="product-image"
                 onClick={async () => {
-                  await router.push(`/artigo/${data.data.descricao}-${data.data.codigo}`); //slug?
+                  await router.push(
+                    `/artigo/${data.data.descricao}-${data.data.codigo}`
+                  ); //slug?
                   //router.reload();
                   console.log("click!");
                 }}
-                style={{width:"140px"}}
               ></img>
             </Box>
             <Typography className="cardproduct-name">
@@ -89,7 +83,7 @@ const ProductItemMain = (data: any) => {
             className="btn-add-cart"
             onClick={() => {
               addCartMessage();
-              //addToCartHandler(data.data);
+              addToCartHandler(data.data);
             }}
           >
             {data.data.precovenda.toFixed(2)}€
